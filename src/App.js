1655fd68b647e5f4257c33beda410e06c09c2794
Anderson Campos Padilha <campospadilha.a@gmail.com>
2025-04-17 16:46:59 -0300

    Adicionada navegabilidade entre os recursos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  {/*para transformar os botões em guias*/}
+  const [activeTab, setActiveTab] = useState('ask-ai');
+
+  const handleTabChange = (tab) => {
+
+    //alert(tab);
+    setActiveTab(tab);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*criação de 3 (três) botões na tela*/}
+      <button
+        className={activeTab === 'ask-ai'? 'active' : ''}
+        onClick={() => handleTabChange('ask-ai')}>
+          Converse com AI
+      </button>
+      <button
+        className={activeTab === 'recipe-generator'? 'active' : ''}
+        onClick={() => handleTabChange('recipe-generator')}>
+          Geração de receitas
+      </button>
+      <button
+        className={activeTab === 'image-generator'? 'active' : ''}
+        onClick={() => handleTabChange('image-generator')}>
+          Geração de imagens
+      </button>
+
+      {/*reação da tela de acordo com o botão executado*/}
+      <div>
+        {activeTab === 'ask-ai' && <h2>Converse com AI</h2>}
+        {activeTab === 'recipe-generator' && <h2>Geração de receitas</h2>}
+        {activeTab === 'image-generator' && <h2>Geração de imagens</h2>}
+      </div>
     </div>
   );
 }
